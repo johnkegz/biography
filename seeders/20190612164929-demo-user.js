16f999571ff85e3ed7e4b4d6fcf2e949974374ff
johnkegz <johnkal24@gmail.com>
2019-06-12 19:56:50 +0300

    add seeders

---
+++ seeders/20190612164929-demo-user.js
@@ -0,0 +1,34 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkInsert('People', [{
+        name: 'John Doe',
+        isBetaMember: false
+      }], {});
+    */
+   return queryInterface.bulkInsert('Users', [{
+    firstName: "John",
+    lastName: "Kalyango",
+    email: "johnkal24@gmail.com",
+    createdAt: "2018-08-16 012:11:52.181+01",
+    updatedAt: "2018-08-16 012:11:52.181+01"
+  }], {});
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkDelete('People', null, {});
+    */
+   return queryInterface.bulkDelete('Users', null, {});
+  }
+};
