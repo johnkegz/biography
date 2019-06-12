8ae5ce61ec7472d2d27ef9034ff1e4f795e592c1
johnkegz <johnkal24@gmail.com>
2019-06-12 10:52:18 +0300

    add middleware functions

--- src/index.js
+++ src/index.js
@@ -5,10 +5,33 @@ import express from 'express';
 import 'dotenv/config';
 
 const app = express();
-//app listens on this port 3000
-const port = 3000; 
+const port = 3000;
+
+//writting middle warefunctions
+const myLogger = (req, res, next) => {
+    console.log("middleware function")
+    next()
+}
+const requestTime = (req, res, next) => {
+    req.requestTime = Date.now()
+    next()
+}
+//application level middlewares
+app.use(myLogger)
+app.use(requestTime)
+//error -handling middleware
+const errorhandler = (err, req, res, next) => {
+    console.log(err.stack)
+    res.statusCode(500).send('internal server error')
+}
+app.use(errorhandler)
 //making a route with the <http method>(<path(route)>, <middle ware function(req, resp, next)>)
-app.get("/", (req, res) => res.send("Hello world"));
+app.get("/", (req, res) => {
+    let responseText = 'request time<br/>'
+    responseText += `<small>time${req.requestTime}</small>`
+
+    res.send(responseText)
+});
 //post route
-app.post("/post", (req, res) => res.send(res.statusCode))
+app.post("/post", (req, res) => res.sendStatus(res.statusCode))
 app.listen(port, () => console.log(`this app listens on port 3000 .${process.env.NAME}`));
