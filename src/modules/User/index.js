d001e656156e15ae0bcb250eec854f8bf01917a3
johnkegz <johnkal24@gmail.com>
2019-07-18 00:08:25 +0300

    add uer login using jwt token

--- src/modules/User/index.js
+++ src/modules/User/index.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import registerUser from './register' 
+import loginUser from './login' 
 
 const router = express.Router();
 
 router.post('/register', registerUser);
+router.post('/login', loginUser);
 
 export default router;
---
+++ src/modules/User/login.js
@@ -0,0 +1,42 @@
+import model from '../../database/models';
+import bcrypt from 'bcryptjs'
+import jwt from 'jsonwebtoken'
+
+const loginUser = (req, res) => {
+    model.User.findOne({where:{email: req.body.email}})
+    .then(user => {
+        if(!user) {
+            return res.status(404).json({email: 'User not found'})
+        }
+        
+        bcrypt.compare(req.body.password, user.password)
+        .then(isMatch => {
+            if(isMatch){
+                //User matched
+                const payload = {
+                    id: user.id,
+                    username: user.username
+                }
+                //sign token
+                jwt.sign(
+                    payload, 
+                    'secreteKey', 
+                    { expiresIn: 3600 }, 
+                    (err, token) =>{
+                        console.log('here man');
+                        return res.json({
+                            success: 'true',
+                            token: `Bearer ${token}`
+                        })
+                    }
+                )
+            } else
+            {   console.log('here ++', isMatch);
+                return res.status(400).json({password: 'wrong password'})
+            }
+        });
+    })
+    .catch(err => res.send(`err ${err}`));
+}
+
+export default loginUser
