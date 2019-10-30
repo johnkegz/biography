5b16b9bef19d55d9b53d8699c4877a35c3aed319
johnkegz <johnkal24@gmail.com>
2019-10-30 10:01:47 +0300

    add environmental variables

--- src/database/config/database.js
+++ src/database/config/database.js
@@ -1,23 +1,23 @@
-// module.exports = {
-//   development: {
-//     username: "postgres",
-//     password: "kegz",
-//     database: "bio",
-//     host: "127.0.0.1",
-//     dialect: "postgres"
-//   },
-//   test: {
-//     username: "root",
-//     password: null,
-//     database: "database_test",
-//     host: "127.0.0.1",
-//     dialect: "mysql"
-//   },
-//   production: {
-//     username: "root",
-//     password: null,
-//     database: "",
-//     host: process.env.HOST || "127.0.0.1",
-//     dialect: "postgres"
-//   }
-// };
+module.exports = {
+  development: {
+    username: "postgres",
+    password: "kegz",
+    database: "bio",
+    host: "127.0.0.1",
+    dialect: "postgres"
+  },
+  test: {
+    username: "root",
+    password: null,
+    database: "database_test",
+    host: "127.0.0.1",
+    dialect: "mysql"
+  },
+  production: {
+    username: process.env.USERNAME,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE,
+    host: process.env.HOST,
+    dialect: "postgres"
+  }
+};
